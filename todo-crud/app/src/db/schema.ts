d3606b0a44466f788018305662833c4d96ca83d9
takuyaw-w <takuya.jamtb@gmail.com>
2024-08-18 02:50:01 +0900

    wip

--- todo-crud/app/src/db/schema.ts
+++ todo-crud/app/src/db/schema.ts
@@ -1,1 +1,21 @@
-export { }
+import { pgTable, serial, timestamp, varchar, text, uniqueIndex, foreignKey, integer } from 'drizzle-orm/pg-core'
+
+const commonTimestamp = {
+    created_at: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow(),
+    updated_at: timestamp('updated_at', { withTimezone: true, mode: 'string' }).defaultNow(),
+    deleted_at: timestamp('deleted_at', { withTimezone: true, mode: 'string' }),
+}
+
+export const statuses = pgTable('statuses', {
+    id: serial('id'),
+    name: varchar('name', { length: 50 }).notNull().unique(),
+    ...commonTimestamp
+})
+
+export const tasks = pgTable('tasks', {
+    id: serial('id'),
+    title: varchar('title', { length: 255 }).notNull(),
+    description: text('text'),
+    status: integer('status').notNull().references(() => statuses.id),
+    ...commonTimestamp
+})
