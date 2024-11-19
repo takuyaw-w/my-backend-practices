0742adeae5031856c8dc541f287ca96ddf38abc4
takuyaw-w <takuya.jamtb@gmail.com>
2024-11-19 23:05:23 +0900

    fix: Modelファイル修正

--- todo-app/app/Models/Comment.php
+++ todo-app/app/Models/Comment.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Task;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'task_id',
+        'comment',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
--- todo-app/app/Models/Task.php
+++ todo-app/app/Models/Task.php
@@ -4,9 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime'
+    ];
+
+    protected $attributes = [
+        'status' => 'pending',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
